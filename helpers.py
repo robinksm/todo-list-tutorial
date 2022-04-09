e9013cfc4cf889ddaa7f721f8c9c175a2e08ac24
Robin Smith <erineks@live.unc.edu>
2022-04-08 20:37:10 -0400

    backup

---
+++ helpers.py
@@ -0,0 +1,9 @@
+class TODO:
+    id: int
+    title: str
+    description: str
+
+    def __init__(self, id: int, title: str, description: str):
+        self.id = id
+        self.title = title
+        self.description = description
